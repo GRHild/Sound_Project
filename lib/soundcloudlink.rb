32eb7c8f47985780e46e92815797a2eb79c183c5
Greg Hild <grhild@gmail.com>
2013-09-25 12:00:57 -0500

    Add version of ruby to gemfile.

--- lib/soundcloudlink.rb
+++ lib/soundcloudlink.rb
@@ -6,5 +6,13 @@ require 'soundcloud'
 
 # Authorizing and linking for Soundcloud
 # create a client object with access token
-client = Soundcloud.new(:access_token => 'YOUR_ACCESS_TOKEN')
+require 'soundcloud'
 
+# create client object with app and user credentials
+client = Soundcloud.new(:client_id => '',
+                        :client_secret => '',
+                        :username => '',
+                        :password => '')
+
+# print authenticated user's username
+puts client.get('/me').username
