87e3ae85c826bbddb29ec4e66642a14fad493afb
Greg Hild <grhild@gmail.com>
2013-09-25 11:18:50 -0500

    Add Gemfile and linked sound file with practice.erb

--- sound.rb
+++ sound.rb
@@ -2,11 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'koala'
-require 'soundcloud'
 
 get '/' do
-  erb :index
+  erb :practice
 end
 
 post '/tracks' do
