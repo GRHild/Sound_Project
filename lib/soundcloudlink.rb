42f539da144313eecae4b92e09dc1df45dd0dda6
Greg Hild <grhild@gmail.com>
2013-09-26 10:27:55 -0500

    Add soundcloud track with many bugs

--- lib/soundcloudlink.rb
+++ lib/soundcloudlink.rb
@@ -1,26 +1,43 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'koala'
-
+require 'soundcloud'
 
 
 module SocialOne
 
   class User
 
-# Initialize a new user.
+# # Initialize a new user.
+#     def initialize
+
+# # Initializes a user via the login token. I'm not sure why its called @graph.    
+#       @graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
+#     end
+
+# # Method that allows a post to go to facebook. Works in IRB.
+#     def post_fb_text(status_update)
+#       @graph.put_connections('me', 'feed', :message => status_update)
+#     end
+#   end
+
+
+
+# register a client with YOUR_CLIENT_ID as client_id_
     def initialize
-
-# Initializes a user via the login token. I'm not sure why its called @graph.    
-      @graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
+      client = SoundCloud.new(:client_id => ENV['CLIENT_ID'])
     end
 
-# Method that allows a post to go to facebook. Works in IRB.
-    def post_fb_text(status_update)
-      @graph.put_connections('me', 'feed', :message => status_update)
+# get 10 hottest tracks
+    def get_song
+      tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
+# print each link
+      tracks.each do |track|
+      puts track.permalink_url
+      end
     end
+
   end
-
 end
 
 
@@ -32,24 +49,3 @@ end
 
 
 
-
-
-
-
-
-
-# module SoundProject
-# # Authorizing and linking for Soundcloud
-# # create a client object with access token
-
-#   class User
-# # create client object with app and user credentials
-# client = Soundcloud.new(:client_id => ENV['CLIENT_KEY']],
-#                         :client_secret => ENV['CLIENT_SECRET'],
-#                         :username => ENV['USERNAME'],
-#                         :password => ENV['PASSWORD'])
-
-# # print authenticated user's username
-# puts client.get('/me').username
-
-#   end
--- sound.rb
+++ sound.rb
@@ -3,17 +3,28 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'koala'
+require 'soundcloud'
 
 require_relative './lib/soundcloudlink.rb'
 
 include SocialOne
+
+# get '/' do
+#   erb :practice
+# end
 
 get '/' do
   erb :practice
 end
 
 post '/facebook' do
-  @user = SocialOne::User.new
-  puts @result = @user.post_fb_text(params[:update])
-  erb :results
+  @user = SoundCloud.new
+  @results = @user.get_song("Evil Friends")
+  erb:results
 end
+
+# post '/facebook' do
+#   @user = SocialOne::User.new
+#   puts @result = @user.post_fb_text(params[:update])
+#   erb :results
+# end
